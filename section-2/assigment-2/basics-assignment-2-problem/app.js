21a71ec65c75556d97fdf0edfdb49b3a4385a4dd
rezafitriaman <rezafitriaman@gmail.com>
2021-02-11 22:02:00 +0100

    add section 2 assigment 2

---
+++ section-2/assigment-2/basics-assignment-2-problem/app.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el: '#assignment',
+    data: {
+        message: 'Event Practice',
+        inputOne: '',
+        inputTwo: ''
+    },
+    methods: {
+        showAlert: function () {
+            const text = (this.inputOne === '') ? 'Hello! I am an alert box!!' : this.inputOne;
+            window.alert(text);
+        },
+        outputOne: function (event) {
+            console.log(event.target.value);
+            this.inputOne = event.target.value;
+        },
+        outputTwo: function (event) {
+            this.inputTwo = event.target.value;
+        }
+    },
+});
