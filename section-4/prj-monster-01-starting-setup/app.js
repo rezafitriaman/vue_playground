c783322e9e8083cd1de451662b23331b1ff4208c
rezafitriaman <rezafitriaman@gmail.com>
2021-02-23 18:39:58 +0100

    add game over screen

--- section-4/prj-monster-01-starting-setup/app.js
+++ section-4/prj-monster-01-starting-setup/app.js
@@ -1,4 +1,4 @@
-const attackDealt = function(min, max) {
+const randomNumber = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min )
 }
 
@@ -10,32 +10,58 @@ const app = new Vue({
         round: 0,
         specialAttackEnable: true,
         specialAttackWaitCounter: 0,
-        gatherEnergyForSpecialAttack: 3
+        gatherEnergyForSpecialAttack: 3 
     },
     computed: {
         canUseSpecialAttack: function() {
-            console.log('canUseSpecialAttack:', this.specialAttackEnable)
             return this.specialAttackEnable
-
+        },
+        gameOver: function() {
+            return this.playerHealth <= 0|| this.monsterHealth <= 0
+        },
+        monsterWon: function() {
+            return this.playerHealth <= 0 && this.monsterHealth > 0
+        },
+        draw: function() { 
+            return this.playerHealth <= 0 && this.monsterHealth <= 0   
         }
     },
     watch: {
         specialAttackWaitCounter: function(value) {
-            console.log(value)
-
             if(value === this.gatherEnergyForSpecialAttack) {
-                console.log('specialAttackEnable', true)
-                this.specialAttackEnable = true
-            }else {
-                console.log('specialAttackEnable', false)
+                this.specialAttackEnable = true 
             }
         }
     },
     methods: {
-        attackMonster: function() {
-            const damageDealt = attackDealt(5, 12)
+        playAgain: function() {
+            this.playerHealth = 100,
+            this.monsterHealth = 100,
+            this.round = 0,
+            this.specialAttackEnable = true,
+            this.specialAttackWaitCounter = 0
+        },
+        surrender: function() {
 
-            if(this.monsterHealth - damageDealt < 0){
+            this.playerHealth = 0
+        },
+        healPlayer: function() {
+            const healPower = randomNumber(12, 20);
+            
+            if(this.playerHealth + healPower >= 100) {
+                this.playerHealth = 100
+            }else {
+                this.playerHealth = this.playerHealth + healPower
+            } 
+
+            this.attackPlayer()
+            this.round = this.round + 1
+            this.specialAttackWaitCounter = this.specialAttackWaitCounter + 1
+        },
+        attackMonster: function() {
+            const damageDealt = randomNumber(5, 12)
+
+            if(this.monsterHealth - damageDealt < 0) {
                 this.monsterHealth = 0
             }else {
                 this.monsterHealth = this.monsterHealth - damageDealt
@@ -44,10 +70,9 @@ const app = new Vue({
             this.attackPlayer()
             this.round = this.round + 1
             this.specialAttackWaitCounter = this.specialAttackWaitCounter + 1
-          
         },
         attackPlayer: function() {
-            const damageDealt = attackDealt(8, 18)
+            const damageDealt = randomNumber(8, 18)
 
             if(this.playerHealth - damageDealt < 0){ 
                 this.playerHealth = 0
@@ -56,9 +81,9 @@ const app = new Vue({
             }
         },
         specialAttackMonster: function() {
-            const damageDealt = attackDealt(12, 24)
+            const damageDealt = randomNumber(12, 24)
 
-            if(this.monsterHealth - damageDealt < 0){
+            if(this.monsterHealth - damageDealt < 0) {
                 this.monsterHealth = 0
             }else {
                 this.monsterHealth = this.monsterHealth - damageDealt
