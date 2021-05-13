3cb4d84f63460eee9b04217cc62776c123e17e86
rezafitriaman <rezafitriaman@gmail.com>
2021-05-13 20:16:59 +0200

    finished 127 on section 10

---
+++ section-10/starting-setup2/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <the-header :title="headerTitle"></the-header>
+  <stored-resources :resources="storedReources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from "./components/learning-resources/StoredResources";
+import TheHeader from "./components/layouts/TheHeader";
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+    StoredResources,
+  },
+  data() {
+    return {
+      headerTitle: 'Remember me',
+      storedReources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official vue.js documention",
+          link: "http://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Lear to google....",
+          link: "http://google.org",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap");
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+</style>
---
+++ section-10/starting-setup2/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <learning-resources
+      v-for="storedReource in resources"
+      :key="storedReource.id"
+      :title="storedReource.title"
+      :description="storedReource.description"
+      :link="storedReource.link"
+      >{{ storedReource.id }}</learning-resources
+    >
+  </ul>
+</template>
+
+<script>
+import LearningResources from "./LearningResources";
+
+export default {
+    components: {
+        LearningResources
+    },
+    props: ['resources']
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
