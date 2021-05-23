c682ef4b50ad17a174e04b6923558ea3b24c5e77
rezafitriaman <rezafitriaman@gmail.com>
2021-05-23 15:54:30 +0200

    finished section 11

---
+++ section-11/starting-setup/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <the-form></the-form>
+</template>
+
+<script>
+import TheForm from './components/TheForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
+}
+</style>
