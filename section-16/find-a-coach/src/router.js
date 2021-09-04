cd34e28e064e9de1f1fca2daeb7532b22e7c2bbd
rezafitriaman <rezafitriaman@gmail.com>
2021-09-04 23:25:35 +0200

    finished 234

--- section-16/find-a-coach/src/router.js
+++ section-16/find-a-coach/src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
+import CoachDetail from './pages/coaches/CoachDetail';
+import CoachesList from './pages/coaches/CoachesList';
+import CoachRegistration from './pages/coaches/CoachRegistration';
+import ContactCoach from './pages/requests/ContactCoach';
+import RequestsReceived from './pages/requests/RequestsReceived';
+import NotFound from './pages/NotFound';
+
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
-        { path: '/coaches:id', component: null,
+        { path: '/coaches', component: CoachesList },
+        { path: '/coaches:id', component: CoachDetail,
             children: [
-                { path: 'contact', component: null}, // coaches/c1/contact
+                { path: 'contact', component: ContactCoach}, // coaches/c1/contact
             ]
         },
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound(.*)', component: null }
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestsReceived },
+        { path: '/:notFound(.*)', component: NotFound }
     ],
 });
 
